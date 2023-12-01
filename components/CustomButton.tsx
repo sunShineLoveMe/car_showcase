b24bb0870905a1fde00974b3c394e8cacb96bc06
sunShineLoveMe <juneandrao@outlook.com>
2023-12-01 10:16:40 +0800

    完善首页车型信息

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,16 @@
 'use client'
-const CustomButton = () => {
+
+import { CustomButtonProps } from "@/types"
+
+const CustomButton = ({ title, btnType,  containerStyles, handleClick } : CustomButtonProps) => {
   return (
     <button
         disabled = {false}
-        type={"button"}
-        className={`custom-btn`}
-        onClick={() => {}}>
+        type={btnType || "button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}>
             <span className={`flex-1`}>
-                title
+                {title}
             </span>
     </button>
   )
